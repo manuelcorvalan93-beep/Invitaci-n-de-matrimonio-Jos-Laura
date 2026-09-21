d811c24db101d47e00b899e4193be0e0ba7d9417
Manuel_99 <manuel.corvalan93@gmail.com>
2026-09-20 21:14:52 -0300

    Update config.js to remove gift details
    Removed gift information and personal message from config.

--- config.js
+++ config.js
@@ -107,24 +107,7 @@ const BODA = {
         dressCode: "Elegante",
 
         regalos:
-            "**Tu presencia es nuestro mejor regalo.** 💜
-
-Lo más importante para nosotros es poder compartir este día tan especial junto a las personas que queremos y que nos acompañen a comenzar esta nueva etapa de nuestras vidas.
-
-Si además desean hacernos un obsequio, lo recibiremos con mucho cariño y gratitud. Para quienes prefieran hacerlo mediante transferencia, dejamos nuestros datos:
-
-**Transferencia bancaria**
-Laura Daniela Urrutia Maldonado
-RUT: 202670334
-Mercado Pago
-Cuenta Vista
-Número de cuenta: 1092911078
-[laura.urrutia0207@gmail.com](mailto:laura.urrutia0207@gmail.com)
-
-Y si prefieren entregarlo personalmente, en la recepción encontrarán **sobres y un baúl**, donde podrán dejarnos su obsequio o unas palabras y buenos deseos para nosotros.
-
-**Gracias por ser parte de este momento tan especial.**
-Su cariño, compañía y buenos deseos significan muchísimo para nosotros. 💜✨",
+            "**Tu presencia es nuestro mejor regalo.** 💜 ",
 
         telefono:
             "+56 9 5300 9921"
